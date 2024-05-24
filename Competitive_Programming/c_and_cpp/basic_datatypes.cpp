d8fe180575ffc8cf0618a887a4980b80a50967f2
Aakash Rana <aakash98523@gmail.com>
2024-05-25 00:00:45 +0545

    program implementing basic datatypes

---
+++ Competitive_Programming/c_and_cpp/basic_datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    long ln;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &n,&ln,&c,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", n,ln,c,f,d);
+    return 0;
+}
+
